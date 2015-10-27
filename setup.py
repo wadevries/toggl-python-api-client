378e5eabb547c18875856c8624440bae416a0b1d
Wouter de Vries <wouter@oxyzen.nl>
2015-10-27 20:07:20 +0100

    Adds basic setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='toggl-python-api-client',
+    version='0.1.0',
+    description='A Python-based REST client to interface with the Toggle API',
+    long_description=long_description,
+    url='https://github.com/mechastorm/toggl-python-api-client',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: APIs',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='time tracking toggl api rest client',
+
+    py_modules=["api_client"],
+    install_requires=['requests'],
+)
